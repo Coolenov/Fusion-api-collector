9014cb3e489eaa74be067496d9e0cc654613860e
Aidar Kulenov <73735193+Coolenov@users.noreply.github.com>
2023-06-20 01:13:18 +0200

    close connect after cicle

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 
 func runservice() {
 	for {
-		db := database.DbConnect("root:firstpass@tcp(fusiondb:3306)/Fusion_db?utf8mb4&loc=Local")
+		db := database.DbConnect("root:firstpass@tcp(db:3306)/Fusion_db?utf8mb4&loc=Local")
 		links, err := database.GetScrapersUrl(db)
 		if err != nil {
 			fmt.Println("Cant get scrapers URL!!!\n Trying more...\n", err)
